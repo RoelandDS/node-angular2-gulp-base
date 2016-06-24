e42c49150a80e7c0a87fd6d868fee13bef379199
Roeland De Smedt <roeland.desmedt@c4j.be>
2016-06-24 16:24:09 +0200

    structure and server.js

---
+++ server.js
@@ -0,0 +1,124 @@
+'use strict';
+const http = require('http');
+const express = require('express');
+const path = require('path');
+const favicon = require('serve-favicon');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+
+const routes = require('./server/router');
+
+const app = express();
+
+// set environment
+const env = app.get('env');
+
+// view engine setup according to environment
+app.set('views', path.join(__dirname, 'server/views/' + env));
+app.set('view engine', 'pug');
+
+// environment config
+const config = require('./config/' + env);
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public/' + env)));
+
+// cross origin headers, delete if not needed
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.header('Access-Control-Allow-Credentials', true);
+  next();
+});
+
+app.use('/', routes);
+
+// catch 404 and forward to error handler
+app.use((req, res, next) => {
+  const err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// development error handler
+// will print stacktrace
+if (env === 'development') {
+  app.use((err, req, res) => {
+    if (err.message === 'invalid domain') {
+      res.redirect('/login');
+  }
+    res.status(err.status || 500);
+    res.json({
+      message: err.message,
+      error: err,
+    });
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use((err, req, res) => {
+  res.status(err.status || 500);
+  res.json({
+    message: err.message,
+    error: {},
+  });
+});
+
+/**
+ * Get port from environment and store in Express.
+ */
+const port = normalizePort(config.port || '4000');
+app.set('port', port);
+
+/**
+ * Create HTTP server.
+ */
+
+const server = http.createServer(app);
+
+/**
+ * Listen on provided port, on all network interfaces.
+ */
+
+server.listen(port);
+server.on('listening', onListening);
+
+/**
+ * Normalize a port into a number, string, or false.
+ */
+
+function normalizePort(val) {
+  const normPort = parseInt(val, 10);
+
+  if (isNaN(normPort)) {
+    // named pipe
+    return val;
+  }
+
+  if (normPort >= 0) {
+    // port number
+    return normPort;
+  }
+
+  return false;
+}
+
+/**
+ * Event listener for HTTP server 'listening' event.
+ */
+
+function onListening() {
+  const addr = server.address();
+  const bind = typeof addr === 'string'
+    ? 'pipe ' + addr
+    : 'port ' + addr.port;
+  console.log('Listening on ', bind);
+}
